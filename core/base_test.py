8a8de40acec5e3ad14e3221b4c2dd6a7bb3e57eb
Виталий <sertelepat12@gmail.com>
2026-08-20 15:55:03 +0500

    Some changes

--- core/base_test.py
+++ core/base_test.py
@@ -9,5 +9,4 @@ def browser():
     options.add_argument("--start-maximized")
     driver = webdriver.Remote(command_executor="http://31.130.148.204:4444", options=options)
     yield driver
-    if driver:
-        driver.quit()
+    driver.quit()
