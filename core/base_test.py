e17be6376a071bf79149217ebcd9580637ba0420
Виталий <sertelepat12@gmail.com>
2026-08-20 16:39:33 +0500

    Some changes for docker

--- core/base_test.py
+++ core/base_test.py
@@ -7,6 +7,9 @@ def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru_RU")
     options.add_argument("--start-maximized")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
     driver = webdriver.Remote(command_executor="http://31.130.148.204:4444", options=options)
     yield driver
     if driver:
