4781db2d44ca3f74926e27641b75d3c87dca9b3d
Виталий <sertelepat12@gmail.com>
2026-08-25 14:39:49 +0500

    Soe changes for page loading

--- core/base_test.py
+++ core/base_test.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru_RU")
+    options.add_argument("--start-maximized")
     driver = webdriver.Remote(command_executor="http://31.130.148.204:4444", options=options)
     try:
         yield driver
